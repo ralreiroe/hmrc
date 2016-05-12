de4a908a36184259df2b6319f83e46a745cae1c4
Ralf Oenning <ralf@invicto.net>
2016-05-12 22:47:23 +0100

    externalize offer functions to make them explicit in tests and configurable

--- src/main/scala/Shop.scala
+++ src/main/scala/Shop.scala
@@ -1,16 +1,32 @@
+import scala.collection.immutable
+
 trait HasPrice {
   val priceInPence: Int
 }
 case class Apple(priceInPence: Int) extends HasPrice
 case class Orange(priceInPence: Int) extends HasPrice
 
-class Shop {
+class Shop(offers: Map[HasPrice, (List[HasPrice] => HasPrice => Int)]) {
+
+  import PriceFunctions._
   def checkout(basket: List[HasPrice]) = {
-    priceXForY(basket, Apple(60),2,1) +
-    priceXForY(basket, Orange(25),3,2)
+    val totalPerProduct = offers.map{
+      t => {
+        val prod = t._1
+        val func: (List[HasPrice]) => (HasPrice) => Int = t._2
+
+        func(basket)(prod)
+      }
+    }
+
+    totalPerProduct.foldLeft(0)(_+_)
   }
 
-  private def _priceXForY(count: Int, hasPrice: HasPrice, leftOfFor: Int, rightOfFor: Int) = (count / leftOfFor) * hasPrice.priceInPence * rightOfFor + (count % leftOfFor) * hasPrice.priceInPence
+}
+
+object PriceFunctions {
+
+  def _priceXForY(count: Int, hasPrice: HasPrice, leftOfFor: Int, rightOfFor: Int) = (count / leftOfFor) * hasPrice.priceInPence * rightOfFor + (count % leftOfFor) * hasPrice.priceInPence
 
   def priceXForY(basket: List[HasPrice], hasPrice: HasPrice, leftOfFor: Int, rightOfFor: Int) = {
     _priceXForY(basket.count(_.equals(hasPrice)), hasPrice, leftOfFor, rightOfFor)
--- src/test/scala/ShopTillTest.scala
+++ src/test/scala/ShopTillTest.scala
@@ -2,43 +2,72 @@ import org.scalatest._
 
 class ShopTillTest extends FlatSpec with Matchers {
 
-  def shop = new Shop()
-
-  "Calculating the total cost" should "work" in {
+  "Calculating the total cost with discounts" should "work" in {
 
     def a = Apple(60)
     def o = Orange(25)
+
+    val appleOffer2For1: List[HasPrice] => HasPrice => Int = basket => prod => {
+      PriceFunctions._priceXForY(basket.count(_.equals(a)), prod, 2, 1)
+    }
+    val orangeOffer3For2: List[HasPrice] => HasPrice => Int = basket => prod => {
+      PriceFunctions._priceXForY(basket.count(_.equals(o)), prod, 3, 2)
+    }
+
+    def shop = new Shop(Map(a -> appleOffer2For1, o -> orangeOffer3For2))
 
     assert(shop.checkout(List(a)) == 60)
     assert(shop.checkout(List(o)) == 25)
     assert(shop.checkout(List(a, a)) == 60)
     assert(shop.checkout(List(a, o, a, a)) == 145)
+    assert(shop.checkout(List(a, o, o, o, a, a)) == 170)
+  }
+
+  "Calculating the total cost without discounts" should "work" in {
+
+    def a = Apple(60)
+    def o = Orange(25)
+
+    val noAppleOffer: List[HasPrice] => HasPrice => Int = basket => prod => {
+      basket.count(_.equals(a)) * prod.priceInPence
+    }
+    val noOrangeOffer: List[HasPrice] => HasPrice => Int = basket => prod => {
+      PriceFunctions._priceXForY(basket.count(_.equals(o)), prod, 1, 1)
+    }
+
+    def shop = new Shop(Map(a -> noAppleOffer, o -> noOrangeOffer))
+
+    assert(shop.checkout(List(a)) == 60)
+    assert(shop.checkout(List(o)) == 25)
+    assert(shop.checkout(List(a, a)) == 120)
+    assert(shop.checkout(List(a, o, a, a)) == 205)
+    assert(shop.checkout(List(a, o, o, o, a, a)) == 255)
   }
 
   "An empty basket" should "work" in {
 
-    assert(shop.checkout(List()) == 0)
+    assert(new Shop(Map()).checkout(List()) == 0)
   }
-  
+
   "Discount functions" should "work" in {
 
     def a = Apple(60)
     def o = Orange(25)
 
-    assert(shop.priceXForY(List(o), o,3,2)==25)
-    assert(shop.priceXForY(List.fill(2)(o), o,3,2)==50)
-    assert(shop.priceXForY(List.fill(6)(o), o,3,2)==100)
-    assert(shop.priceXForY(List.fill(16)(o), o,3,2)==275)
-    assert(shop.priceXForY(List.fill(17)(o), o,3,2)==300)
+    assert(PriceFunctions.priceXForY(List(o), o,3,2)==25)
+    assert(PriceFunctions.priceXForY(List.fill(2)(o), o,3,2)==50)
+    assert(PriceFunctions.priceXForY(List.fill(6)(o), o,3,2)==100)
+    assert(PriceFunctions.priceXForY(List.fill(16)(o), o,3,2)==275)
+    assert(PriceFunctions.priceXForY(List.fill(17)(o), o,3,2)==300)
 
-    assert(shop.priceXForY(List.fill(1)(a), a,2,1)==60)
-    assert(shop.priceXForY(List.fill(2)(a), a,2,1)==60)
-    assert(shop.priceXForY(List.fill(3)(a), a,2,1)==120)
-    assert(shop.priceXForY(List.fill(4)(a), a,2,1)==120)
-    assert(shop.priceXForY(List.fill(5)(a), a,2,1)==180)
-    assert(shop.priceXForY(List.fill(6)(a), a,2,1)==180)
-    assert(shop.priceXForY(List.fill(16)(a), a,2,1)==480)
-    assert(shop.priceXForY(List.fill(17)(a), a,2,1)==540)
+    assert(PriceFunctions.priceXForY(List.fill(1)(a), a,2,1)==60)
+    assert(PriceFunctions.priceXForY(List.fill(2)(a), a,2,1)==60)
+    assert(PriceFunctions.priceXForY(List.fill(3)(a), a,2,1)==120)
+    assert(PriceFunctions.priceXForY(List.fill(4)(a), a,2,1)==120)
+    assert(PriceFunctions.priceXForY(List.fill(5)(a), a,2,1)==180)
+    assert(PriceFunctions.priceXForY(List.fill(6)(a), a,2,1)==180)
+    assert(PriceFunctions.priceXForY(List.fill(16)(a), a,2,1)==480)
+    assert(PriceFunctions.priceXForY(List.fill(17)(a), a,2,1)==540)
 
   }
 }
